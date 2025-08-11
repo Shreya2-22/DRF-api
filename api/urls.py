bf7e43a8e3950176bc08f00ce44885f450e71fc7
Shreya2-22 <kshreya22@tbc.edu.np>
2025-08-11 21:20:19 +0545

    updating and deleting data

--- api/urls.py
+++ api/urls.py
@@ -3,8 +3,7 @@ from . import views
 
 urlpatterns = [
     path('products/', views.ProductListCreateAPIView.as_view(), name='product-list-create'),
-    
-    path('products/<int:product_id>/', views.ProductDetailAPIView.as_view(), name='product-detail'),
+    path('products/<int:product_id>/', views.ProductRetrieveUpdateDestroyAPIView.as_view(), name='product-detail'),
 
     path('products/info/', views.ProductInfoAPIView.as_view(), name='product-info'),
 
--- api/views.py
+++ api/views.py
@@ -20,10 +20,16 @@ class ProductListCreateAPIView(generics.ListCreateAPIView):
         return super().get_permissions()
 
 
-class ProductDetailAPIView(generics.RetrieveAPIView):
+class ProductRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     lookup_url_kwarg='product_id'
+
+    def get_permissions(self):
+        self.permission_classes = [AllowAny]
+        if self.request.method in ['PUT', 'PATCH', 'DELETE']:
+            self.permission_classes = [IsAdminUser]
+        return super().get_permissions()
 
 
 class OrderListAPIView(generics.ListAPIView):
